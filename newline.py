aedfb09844dae4a5bc79744d0c4bc360618b013b
Jason Callaway <jasoncallaway@google.com>
2019-03-12 22:57:43 -0400

    added utiltity to prep old results for bq load

---
+++ newline.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+
+def fix_dict(d):
+    n = {}
+    for key in d.keys():
+        new_key = key.replace(' ', '_').replace('-', '_')
+        if isinstance(d[key], dict):
+            k = d[key]
+            n[new_key] = fix_dict(d[key])
+        else:
+            n[new_key] = d[key]
+    return n
+
+
+with open(sys.argv[1], 'r') as f:
+    file_list = f.readlines()
+
+for filename in file_list:
+    with open(filename.rstrip(), 'r') as f, open('./newlinedelimited.json', 'a') as o:
+        try:
+            j = json.loads(f.read())
+            k = fix_dict(j)
+            # print(str(k))
+            o.write(str(k))
+        except Exception as e:
+            continue
